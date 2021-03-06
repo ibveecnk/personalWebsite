6f9bae45097f0436c2890c6c59418a792ad9930c
Iven Beck <ivenbeck@outlook.de>
2021-03-06 18:03:02 +0100

    Add live gh api fetching

--- pages/projects.js
+++ pages/projects.js
@@ -3,29 +3,29 @@ import Header from "../components/header";
 import Title from "../components/title";
 
 import { Octokit } from "@octokit/rest";
+import useSWR from "swr";
 const octokit = new Octokit();
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const res = await await octokit.users.getByUsername({ username: "ibveecnk" });
-  return {
-    props: {
-      gitHubData: res,
-    },
-    revalidate: 10,
-  };
+function gitHubData() {
+  const { data, error } = useSWR("https://api.github.com/users/ibveecnk");
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return (
+    <div>
+      <img width="100px" height="100px" src={data.avatar_url}></img>
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
 }
 
-export default function Home({ gitHubData }) {
+export default function Home() {
   return (
     <div className="Wrapper">
       <Header page="projects"></Header>
       <div className="cWrapper">
         <Title text="Projects"></Title>
         <p>Here you can find my current projects.</p>
-        <p>{"Fetched at " + gitHubData.headers.date}</p>
+        {gitHubData()}
       </div>
     </div>
   );
