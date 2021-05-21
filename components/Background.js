a9be86f3af68f6071e5b80babd5ad3b3deaa354e
Iven B <ivenbeck@web.de>
2021-05-21 11:47:08 +0200

    Update Background.js

--- components/Background.js
+++ components/Background.js
@@ -4,7 +4,7 @@ import styles from "../styles/Background.module.scss";
 export default function Background(props) {
   return (
     <>
-      <video src="/background.mp4" autoPlay loop className={styles.video} />
+      <video src="/background.mp4" autoPlay loop muted className={styles.video} />
     </>
   );
 }
