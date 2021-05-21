e75018fe50c25fb0c7562e71acd456cacef54c58
Iven Beck <ivenbeck@outlook.de>
2021-05-21 12:17:22 +0200

    Mute video so autoplay works, fix video to site

--- components/Background.js
+++ components/Background.js
@@ -4,7 +4,15 @@ import styles from "../styles/Background.module.scss";
 export default function Background(props) {
   return (
     <>
-      <video src="/background.mp4" autoPlay loop muted className={styles.video} />
+      <div className={styles.bg}>
+        <video
+          src="/background.mp4"
+          autoPlay
+          loop
+          muted
+          className={styles.video}
+        />
+      </div>
     </>
   );
 }
